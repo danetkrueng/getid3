620ad438c4f1730a7a1dbfa5b2d9ef2646ecbb1c
Danet Krueng <danet@web-essentials.asia>
2014-08-19 14:38:42 +0700

    [TASK] Initialize extension getid3

---
+++ ext_emconf.php
@@ -0,0 +1,48 @@
+<?php
+
+########################################################################
+# Extension Manager/Repository config file for ext "getid3".
+#
+# Auto generated 09-12-2010 22:36
+#
+# Manual updates:
+# Only the data in the array - everything else is removed by next
+# writing. "version" and "dependencies" must not be touched!
+########################################################################
+
+$EM_CONF[$_EXTKEY] = array(
+	'title' => 'getID3',
+	'description' => 'This extension provides the getID3 library for use in other extensions',
+	'category' => 'misc',
+	'shy' => 0,
+	'dependencies' => '',
+	'conflicts' => '',
+	'priority' => '',
+	'loadOrder' => '',
+	'module' => '',
+	'state' => 'beta',
+	'uploadfolder' => 0,
+	'createDirs' => '',
+	'modify_tables' => '',
+	'clearCacheOnLoad' => 0,
+	'lockType' => '',
+	'author' => 'Thomas Murphy',
+	'author_email' => 'murphy@spreekonzept.de',
+	'author_company' => '',
+	'CGLcompliance' => '',
+	'CGLcompliance_note' => '',
+	'version' => '0.0.1',
+	'constraints' => array(
+		'depends' => array(
+			'typo3' => '3.5.0-0.0.0',
+			'php' => '3.0.0-0.0.0',
+		),
+		'conflicts' => array(
+		),
+		'suggests' => array(
+		),
+	),
+	'_md5_values_when_last_written' => 'a:64:{s:9:"ChangeLog";s:4:"d911";s:10:"README.txt";s:4:"ee2d";s:12:"ext_icon.gif";s:4:"1bdc";s:31:"classes/extension.cache.dbm.php";s:4:"ebf1";s:33:"classes/extension.cache.mysql.php";s:4:"595b";s:22:"classes/getid3.lib.php";s:4:"0eb9";s:18:"classes/getid3.php";s:4:"5093";s:31:"classes/module.archive.gzip.php";s:4:"7f81";s:30:"classes/module.archive.rar.php";s:4:"556a";s:31:"classes/module.archive.szip.php";s:4:"7023";s:30:"classes/module.archive.tar.php";s:4:"e852";s:30:"classes/module.archive.zip.php";s:4:"c2d3";s:34:"classes/module.audio-video.asf.php";s:4:"c793";s:35:"classes/module.audio-video.bink.php";s:4:"056b";s:39:"classes/module.audio-video.matroska.php";s:4:"a09d";s:35:"classes/module.audio-video.mpeg.php";s:4:"21e1";s:34:"classes/module.audio-video.nsv.php";s:4:"4d87";s:40:"classes/module.audio-video.quicktime.php";s:4:"180d";s:35:"classes/module.audio-video.real.php";s:4:"d413";s:35:"classes/module.audio-video.riff.php";s:4:"a1db";s:34:"classes/module.audio-video.swf.php";s:4:"4361";s:28:"classes/module.audio.aac.php";s:4:"7e99";s:28:"classes/module.audio.ac3.php";s:4:"20a8";s:27:"classes/module.audio.au.php";s:4:"ebc4";s:28:"classes/module.audio.avr.php";s:4:"9f36";s:29:"classes/module.audio.bonk.php";s:4:"55a9";s:29:"classes/module.audio.flac.php";s:4:"71c2";s:27:"classes/module.audio.la.php";s:4:"7f2d";s:29:"classes/module.audio.lpac.php";s:4:"ed95";s:29:"classes/module.audio.midi.php";s:4:"748a";s:28:"classes/module.audio.mod.php";s:4:"25b7";s:31:"classes/module.audio.monkey.php";s:4:"4af2";s:28:"classes/module.audio.mp3.php";s:4:"682f";s:28:"classes/module.audio.mpc.php";s:4:"90f0";s:28:"classes/module.audio.ogg.php";s:4:"8683";s:34:"classes/module.audio.optimfrog.php";s:4:"5b72";s:29:"classes/module.audio.rkau.php";s:4:"897a";s:32:"classes/module.audio.shorten.php";s:4:"7c7a";s:28:"classes/module.audio.tta.php";s:4:"8bc8";s:28:"classes/module.audio.voc.php";s:4:"2a9d";s:28:"classes/module.audio.vqf.php";s:4:"32ff";s:32:"classes/module.audio.wavpack.php";s:4:"9709";s:30:"classes/module.graphic.bmp.php";s:4:"4c6f";s:30:"classes/module.graphic.gif.php";s:4:"ecc4";s:30:"classes/module.graphic.jpg.php";s:4:"2dc2";s:30:"classes/module.graphic.pcd.php";s:4:"82c4";s:30:"classes/module.graphic.png.php";s:4:"89e4";s:31:"classes/module.graphic.tiff.php";s:4:"f80a";s:27:"classes/module.misc.exe.php";s:4:"88a1";s:27:"classes/module.misc.iso.php";s:4:"33e0";s:29:"classes/module.tag.apetag.php";s:4:"68bc";s:28:"classes/module.tag.id3v1.php";s:4:"1d5f";s:28:"classes/module.tag.id3v2.php";s:4:"7f34";s:30:"classes/module.tag.lyrics3.php";s:4:"f033";s:24:"classes/write.apetag.php";s:4:"0c1b";s:23:"classes/write.id3v1.php";s:4:"cd9f";s:23:"classes/write.id3v2.php";s:4:"4544";s:25:"classes/write.lyrics3.php";s:4:"e2b4";s:26:"classes/write.metaflac.php";s:4:"f9f3";s:17:"classes/write.php";s:4:"5a82";s:22:"classes/write.real.php";s:4:"1403";s:31:"classes/write.vorbiscomment.php";s:4:"36ce";s:19:"doc/wizard_form.dat";s:4:"d6df";s:20:"doc/wizard_form.html";s:4:"397b";}',
+);
+
+?>
